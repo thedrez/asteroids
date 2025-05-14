f5a4d4f7192d76863da1f2bfe308f4d8d3ea9f0b
thedrez <thedrez@icloud.com>
2025-05-14 11:15:41 -0600

    Ch2-L5 added player sprite class

--- main.py
+++ main.py
@@ -3,21 +3,36 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
+
 
 def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+   
+    # Create a Game Clock
     game_clock = pygame.time.Clock()
     dt = 0
 
+    # Create a Player object
+    player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+
+    # Setup the Game Event Loop
     game_loop = True
     while game_loop:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        # blanking the screen??
         screen.fill("black")
+
+        # have the player draw itself into the screen buffer
+        player.draw(screen)
+
+        # causes screen buffer to be drawn
         pygame.display.flip()
 
         # Limit framerate to 60fps
