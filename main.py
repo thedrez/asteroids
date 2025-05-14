4c5cde382ea115d2b9abd2f831d6cb4ac8f2f7b9
thedrez <thedrez@icloud.com>
2025-05-14 10:29:36 -0600

    Ch2-L2 added game loop

--- main.py
+++ main.py
@@ -8,6 +8,16 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    game_loop = True
+    while game_loop:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
+
 
 if __name__ == "__main__":
     main()
