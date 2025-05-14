6520a94890b500528129638d6103d6c9b9a56f4b
thedrez <thedrez@icloud.com>
2025-05-14 10:40:49 -0600

    Ch2-L3 framerate limit 60fps

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_clock = pygame.time.Clock()
+    dt = 0
 
     game_loop = True
     while game_loop:
@@ -17,6 +19,7 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = game_clock.tick(60)/1000
 
 
 if __name__ == "__main__":
